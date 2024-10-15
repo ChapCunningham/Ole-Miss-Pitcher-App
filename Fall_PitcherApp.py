f778db7d610842fea274878c6f57ea6cbebeae5f
ChapCunningham <chapcunningham@gmail.com>
2024-10-15 17:34:16 -0500

    Update Fall_PitcherApp.py

--- Fall_PitcherApp.py
+++ Fall_PitcherApp.py
@@ -108,7 +108,7 @@ def plot_heatmaps(pitcher_name, batter_side, strikes, balls):
         ax.set_ylabel('')
         
         # Set pitch type as title
-        ax.set_title(f"{pitch_type}", fontsize=12)
+        ax.set_title(f"{pitch_type}", fontsize=24)
 
         # Equal aspect ratio
         ax.set_aspect('equal', adjustable='box')
